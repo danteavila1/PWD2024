29f5ff8fee6fa7c765f1a96bb080fd28f1029316
Dante Avila <danteavila1@hotmail.com>
2024-08-20 19:17:16 -0300

    ej2 tp1 terminado

--- TP1/ej1/vista/ej1.php
+++ TP1/ej1/vista/ej1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ej1</title>
 </head>
 <body>
     <form name="form1" method="get" action="../control/vernumero.php">
